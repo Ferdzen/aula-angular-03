5c0464a99be9ca8a5931ad1fdde8c4c853f1b317
Ferdzen <fernandaivp63@gmail.com>
2026-07-25 17:29:52 +0000

    Introduz favorito.Service.getAll

--- apps/front/src/app/services/favorito/favorito.service.ts
+++ apps/front/src/app/services/favorito/favorito.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@angular/core';
+import { IFavorito } from '@nx-monorepo/comum'
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { API_BASE } from '../../app.config';
+import { Observable, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class FavoritoService {
 
-  constructor() { }
+    private httpClient = inject(HttpClient);
+    private apiBase = inject(API_BASE)
 
-  
+    /**
+     * Recupera a lista de favoritos do backend
+     * @returns Observable Compartilhado e com replay do `array` de `IFavorito`
+     */
+
+    public getAll(): Observable<IFavorito[]> {
+      return this.httpClient.get<IFavorito[]>(`${this.apiBase}/favorito`).pipe(
+        shareReplay(),
+      )
+    }
 }
