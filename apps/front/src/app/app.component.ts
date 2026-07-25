a284aaa2c352ab1a4a52f185959f4674b235ce69
Ferdzen <fernandaivp63@gmail.com>
2026-07-25 13:02:40 +0000

    Configura roteamento basico

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [
     RouterModule,
-  ],
+],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- apps/front/src/app/app.routes.ts
+++ apps/front/src/app/app.routes.ts
@@ -1,4 +1,28 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 export const appRoutes: Route[] = [
+  {
+    path: '', redirectTo: '/home',
+    pathMatch: 'full'
+  },
+
+  {
+    title: 'Seus Favoritos',
+    path: 'home', component: HomeComponent },
+  {
+    title: 'Sobre esta aplicação',
+    path: 'sobre', component: SobreComponent
+  },
+  {
+    title: 'Página não encontrada',
+    path: 'nao-encontrada', component: SobreComponent
+    // fazer um componente de página não encontrada
+  },
+  {
+    path: '**', redirectTo: '/nao-encontrada'
+  }
+
+
 ];
